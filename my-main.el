a64dfec92a4c7b478a2fb90efe8346b45f4ee06e
Alexander Kojevnikov <alexander@kojevnikov.com>
2011-03-17 10:20:16 +0800

    Erlang support

--- my-main.el
+++ my-main.el
@@ -108,3 +108,10 @@
       cperl-continued-statement-offset 0
       cperl-indent-parens-as-block t
       cperl-tab-always-indent t)
+
+;; Erlang
+(setq load-path (cons "/usr/local/lib/erlang/lib/tools-2.6.6.2/emacs" load-path))
+(setq erlang-root-dir "/usr/local/lib/erlang")
+(setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
+(require 'erlang-start)
+(require 'erlang-flymake)
