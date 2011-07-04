a4f3e03db2c288845c2dd209a49bf2c79b0e0139
Alexander Kojevnikov <alexander@kojevnikov.com>
2011-07-04 10:25:37 +0800

    Treat .cl files as .c

--- my-main.el
+++ my-main.el
@@ -52,6 +52,7 @@
 ;; C
 (require 'cc-mode)
 (setq auto-mode-alist (cons '("\\.cu$" . c-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.cl$" . c-mode) auto-mode-alist))
 (define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
 (add-hook 'c-mode-common-hook (
     lambda ()
