f6af7fd3b68059a39a8667332d0d4d4e312b71b0
Alexander Kojevnikov <alexander@kojevnikov.com>
2011-08-19 12:04:00 +0800

    Use csharp-mode.el from ports

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 * misc/color-theme.el
 * math/ess
+* lang/csharp-mode.el
 * lang/haskell-mode.el
 * lang/erlang: `ln -s` the installed emacs mode to /usr/local/share/emacs/site-lisp
 * devel/psvn
--- init-csharp.el
+++ init-csharp.el
@@ -1,5 +1,4 @@
 (autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
-(setq auto-mode-alist
-  (append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
+(setq auto-mode-alist (append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
 
 (provide 'init-csharp)
