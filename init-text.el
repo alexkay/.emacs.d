21b0ef6060ab5b59e67827b1f215bc70f38e0e53
Alexander Kojevnikov <alexander@kojevnikov.com>
2012-05-21 11:48:20 -0700

    Set text size

--- init-text.el
+++ init-text.el
@@ -4,5 +4,6 @@
 (ispell-change-dictionary "british" t)
 (add-hook 'text-mode-hook 'flyspell-mode 1)
 (add-hook 'python-mode-hook 'flyspell-prog-mode 1)
+(set-face-attribute 'default nil :height  110)
 
 (provide 'init-text)
