5f2327a574d876853abcbdf52d1b8aa0f7d033f8
Alexander Kojevnikov <alexander@kojevnikov.com>
2012-05-25 16:52:51 -0700

    Jump to Python library docs

--- init-python.el
+++ init-python.el
@@ -29,4 +29,24 @@
                '("\\.py\\'" flymake-pylint-init)))
 ;(add-hook 'python-mode-hook 'flymake-mode)
 
+;; http://code.naeseth.com/python/libjump/
+(defun better-current-word ()
+  "Similar to current-word, but include dot and don't include dash.  This is
+   better suited to non-LISP programming."
+  (save-excursion
+    (re-search-backward "[^A-Za-z0-9_\\.]")
+    (if (re-search-forward "\\([A-Za-z0-9_\\.]+\\)" nil t)
+      (match-string 1) "")))
+
+(defun read-word-target (prompt history-symbol)
+  (let ((default (better-current-word)))
+    (read-from-minibuffer prompt default nil nil history-symbol)))
+
+(defun erics-libjump (query)
+  (interactive (list (read-word-target "Python stdlib search: " nil)))
+  (call-process "firefox" nil nil nil
+    (concat "http://code.naeseth.com/python/libjump/search?q=" query)))
+(add-hook 'python-mode-hook
+  (function (lambda () (local-set-key "\C-hp" 'erics-libjump))))
+
 (provide 'init-python)
