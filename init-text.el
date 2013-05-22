1716aac534a578906efe23e796c24b51a94d3e21
Alexander Kojevnikov <alexander@kojevnikov.com>
2013-05-22 15:52:53 -0700

    Change ispell language

--- init-text.el
+++ init-text.el
@@ -1,7 +1,7 @@
 ;; Text and spelling.
 (setq default-major-mode 'text-mode)
 (setq ispell-program-name "aspell")
-(ispell-change-dictionary "british" t)
+(ispell-change-dictionary "american" t)
 (add-hook 'text-mode-hook 'flyspell-mode 1)
 (add-hook 'python-mode-hook 'flyspell-prog-mode 1)
 (set-face-attribute 'default nil :height  110)
